fd6efdfde4524ae3e7085ee59fc381f30bb42dbc
Daniel Redondo Velasco <danielredondovelasco@gmail.com>
2025-04-11 11:59:37 +0200

    Actualización del toDo

--- src/MAIN/Main.java
+++ src/MAIN/Main.java
@@ -12,26 +12,26 @@ import static DB.UTIL.CrearConn.crearConexion;
 
 public class Main {
 
-    private static Connection conn;
+        private static Connection conn;
 
-public static void main(String[] args) {
+        public static void main(String[] args) {
 
-    //Creamos la conexion a la base de datos
-    if (crearConexion()) {
-        //Obtenemos la conexion
-        conn = GestorConexion.getConexion();
-        //Llamamos a la vista de inicio de sesion para iniciar el programa
-        SwingUtilities.invokeLater(() -> new InicioSesion_Vista(conn).setVisible(true));
-    } else{
-        //En caso de error al crear la conexion, mostramos un mensaje de error
-        SwingUtilities.invokeLater(() -> new Error_INICIAR_BD().setVisible(true));
-    }
+                //Creamos la conexion a la base de datos
+                if (crearConexion()) {
+                        //Obtenemos la conexion
+                        conn = GestorConexion.getConexion();
+                        //Llamamos a la vista de inicio de sesion para iniciar el programa
+                        SwingUtilities.invokeLater(() -> new InicioSesion_Vista(conn).setVisible(true));
+                } else{
+                        //En caso de error al crear la conexion, mostramos un mensaje de error
+                        SwingUtilities.invokeLater(() -> new Error_INICIAR_BD().setVisible(true));
+                }
 
-    // Agregar un Shutdown Hook para cerrar la conexión al cerrar la aplicación
-    //Crea un hilo que se ejecuta al cerrar la aplicación para cerrar la conexión a la base de datos
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-        System.out.println("Cerrando la conexión a la base de datos...");
-        System.out.println(Mensajes.getMensaje(GestorConexion.cerrarConexion()));
-    }));    }
+                // Agregar un Shutdown Hook para cerrar la conexión al cerrar la aplicación
+                //Crea un hilo que se ejecuta al cerrar la aplicación para cerrar la conexión a la base de datos
+                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                        System.out.println("Cerrando la conexión a la base de datos...");
+                        System.out.println(Mensajes.getMensaje(GestorConexion.cerrarConexion()));
+                }));    }
 
 }
