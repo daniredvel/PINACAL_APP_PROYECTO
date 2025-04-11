818e10130ae4f37503ed5818a301d7b1dcd133d9
Daniel Redondo Velasco <danielredondovelasco@gmail.com>
2025-04-11 13:08:59 +0200

    Ejemplo JavaDoc

--- src/CONTROLLER/CRUD/PUBLICACION/AddPublicacion.java
+++ src/CONTROLLER/CRUD/PUBLICACION/AddPublicacion.java
@@ -15,6 +15,27 @@ import java.util.logging.Logger;
 
 import static DB.UTIL.CrearConn.conn;
 
+
+/**
+ * Clase encargada de gestionar la creación de publicaciones en la base de datos.
+ * Proporciona métodos para insertar nuevos registros en la tabla de publicaciones.
+ *
+ * <p>Esta clase se conecta a la base de datos y realiza una operación de inserción (INSERT)
+ * en la tabla `PUBLICACIONES` para crear una nueva entrada.</p>
+ *
+ * <strong>Dependencias:</strong>
+ * <ul>
+ *     <li>La clase depende de los paquetes {@link DB.UTIL.GestorConexion}, {@link MODEL.Publicacion} y {@link VIEW.ERROR.Error_INICIAR_BD}.</li>
+ *     <li>Utiliza el paquete estándar de <code>java.sql</code> para conectarse y operar con la base de datos.</li>
+ * </ul>
+ *
+ * @author DANIEL REDONDO VELASCO
+ * @version 1.0
+ * @since 2025
+ */
+
+
+
 public class AddPublicacion {
     private static final Logger LOGGER = Logger.getLogger(AddPublicacion.class.getName());
 
@@ -50,3 +71,4 @@ public class AddPublicacion {
         }
     }
 }
+
